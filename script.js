f39e307ba65102fda16dd6e93a5f0463db0723d5
Kevin Bui <kevinbuui@hotmail.com>
2025-02-12 20:35:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function moveRandomEl(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+  }
+  
+  const moveRandom = document.querySelector("#move-random");
+  
+  moveRandom.addEventListener("mouseenter", function (e) {
+    moveRandomEl(e.target);
+  });
+
+  $(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
